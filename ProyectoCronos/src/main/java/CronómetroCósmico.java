91d284588db1eb052be0cfb5d8cf8e6d8d20945a
maridilo <146822054+maridilo@users.noreply.github.com>
2024-02-21 13:01:01 +0100

    commit

--- ProyectoCronos/src/main/java/CronómetroCósmico.java
+++ ProyectoCronos/src/main/java/CronómetroCósmico.java
@@ -17,6 +17,19 @@ public void convertirYMostrarTiempo(double segundosTierra) {
     double años = (int) añosPlaneta;
     double días = (añosPlaneta - años) * 365.25;
     System.out.println(segundosTierra + " segundos en la Tierra equivalen a " + años + " años y " + días + " días en " + nombrePlaneta);
+
+    System.out.println("El tiempo en " + nombrePlaneta + " es " + segundosPlaneta + " segundos");
+}
+public static void main(String[] args) {
+    //Definir la duración de un dia y un año en el nuevo planeta
+    double duraciónDíaPlaneta = 20; // en segundos
+    double duraciónAñoPlaneta = 400; // en segundos
+    //Crear un cronómetro cósmico para el nuevo planeta
+    CronómetroCósmico cronómetro = new CronómetroCósmico("Nuevo Planeta", duraciónDíaPlaneta, duraciónAñoPlaneta);
+    //Convertir y mostrar el tiempo para 1 dia en la Tierra
+    cronómetro.convertirYMostrarTiempo(DURACIÓN_DÍA_TIERRA);
+    //Convertir y mostrar el tiempo para 1 año en la Tierra
+    cronómetro.convertirYMostrarTiempo(DURACIÓN_AÑO_TIERRA);
 }
 }
 
