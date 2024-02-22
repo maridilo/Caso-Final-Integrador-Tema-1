aa90310660e54b0958cc43cefb128a5e6f3299d3
maridilo <146822054+maridilo@users.noreply.github.com>
2024-02-22 18:58:58 +0100

    commit

--- ProyectoCronos/src/main/java/Planificadordetareas.java
+++ ProyectoCronos/src/main/java/Planificadordetareas.java
@@ -43,12 +43,12 @@ public class Planificadordetareas {
         }
         System.out.println();
     }
-public static int calcularCargaOptima(int[] duraciones, int[] disponibilidadTripulacion) {
-    int cargaOptima = 0;
-    for (int i = 0; i < duraciones.length; i++) {
-        cargaOptima += duraciones[i];
+    public static int calcularCargaOptima(int[] duraciones, int[] disponibilidadTripulacion) {
+        int cargaOptima = 0;
+        for (int i = 0; i < duraciones.length; i++) {
+            cargaOptima += duraciones[i];
+        }
+        return cargaOptima;
     }
-    return cargaOptima;
-}
 }
 
