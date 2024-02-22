d9f6e1c0379724e87bcd71e2d800484ecbac3e14
maridilo <146822054+maridilo@users.noreply.github.com>
2024-02-22 18:25:20 +0100

    commit

--- ProyectoCronos/src/main/java/Planificadordetareas.java
+++ ProyectoCronos/src/main/java/Planificadordetareas.java
@@ -1,2 +1,7 @@
 public class Planificadordetareas {
+    public static void main(String[] args) {
+        // Definir tareas y duraciones
+        String[] tareas = {"Tarea 1", "Tarea 2", "Tarea 3", "Tarea 4", "Tarea 5"};
+        int[] duraciones = {10, 20, 30, 40, 50}; // en minutos
+    }
 }
