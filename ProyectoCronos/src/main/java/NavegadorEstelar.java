ba7cc89d4c43c824d303248e408c98eb31de8946
maridilo <146822054+maridilo@users.noreply.github.com>
2024-02-23 20:18:08 +0100

    commit

--- ProyectoCronos/src/main/java/NavegadorEstelar.java
+++ ProyectoCronos/src/main/java/NavegadorEstelar.java
@@ -1,2 +1,25 @@
 public class NavegadorEstelar {
+    public static void main(String[] args) {
+        // Definicion del terreno y obstáculos mediante matrices
+        int[][] terreno = {
+            {0, 0, 1, 0, 0},
+            {0, 1, 0, 1, 0},
+            {0, 0, 1, 0, 0},
+            {0, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0}
+        };
+        visualizarTerreno(terreno);
+        // Definicion de construcciones y planificacion de rutas mediante matrices
+        int[][] construcciones = {
+            {0, 0, 0, 0, 0},
+            {0, 1, 0, 1, 0},
+            {0, 0, 1, 0, 0},
+            {0, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0}
+        };
+        // Multiplicar matrices para obtener rutas y recursos
+        int[][] rutasOptimizadas = multiplicarMatrices(terreno, construcciones);
+        visualizarRutas(rutasOptimizadas);
+    }
+
 }
