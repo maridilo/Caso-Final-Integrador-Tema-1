48086385d60c9720e1f535bf0fdf15d97c61878a
maridilo <146822054+maridilo@users.noreply.github.com>
2024-02-23 20:42:05 +0100

    commit

--- ProyectoCronos/src/main/java/NavegadorEstelar.java
+++ ProyectoCronos/src/main/java/NavegadorEstelar.java
@@ -21,5 +21,36 @@ public class NavegadorEstelar {
         int[][] rutasOptimizadas = multiplicarMatrices(terreno, construcciones);
         visualizarRutas(rutasOptimizadas);
     }
-
+public static void visualizarTerreno(int[][] terreno) {
+    System.out.println("Terreno y obstaculos:");
+    for (int[] fila : terreno) {
+        for (int celda : fila) {
+            System.out.print(celda + " ");
+        }
+        System.out.println();
+    }
+}
+public static void visualizarRutas(int[][] rutas) {
+    System.out.println("Rutas optimizadas:");
+    for (int[] fila : rutas) {
+        for (int celda : fila) {
+            System.out.print(celda + " ");
+        }
+        System.out.println();
+    }
+}
+public static int[][] multiplicarMatrices(int[][] matriz1, int[][] matriz2) {
+    int filas1 = matriz1.length;
+    int columnas1 = matriz1[0].length;
+    int columnas2 = matriz2[0].length;
+    int[][] resultado = new int[filas1][columnas2];
+    for (int i = 0; i < filas1; i++) {
+        for (int j = 0; j < columnas2; j++) {
+            for (int k = 0; k < columnas1; k++) {
+                resultado[i][j] += matriz1[i][k] * matriz2[k][j];
+            }
+        }
+    }
+    return resultado;
+}
 }
