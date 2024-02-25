1bcee487a3a88c36be781b8cae57aa1101105098
maridilo <146822054+maridilo@users.noreply.github.com>
2024-02-25 17:28:34 +0100

    commit

--- ProyectoCronos/src/main/java/ComunicadorInterplanetario.java
+++ ProyectoCronos/src/main/java/ComunicadorInterplanetario.java
@@ -5,3 +5,20 @@ private ArrayList<String> mensajes;
 public ComunicadorInterplanetario() {
     mensajes = new ArrayList<>();
 }
+public void enviarMensaje(String mensaje) {
+    // Simular envio de mensaje desde la Tierra a la base espacial
+    mensajes.add(traducirmensaje(mensaje));
+}
+public void recibirMensajes() {
+    // Simular recepcion de mensajes en la base espacial
+    mensajes.add(traducirMensaje(mensaje));
+    }
+    private String traducirMensaje(String mensaje) {
+        // Traducir mensaje de un idioma a otro
+        int conteoVocales = contarVocales(mensaje);
+        String mensajeInvertido = invertirMensaje(mensaje);
+        boolean esPalindromo = verificarPalindromo(mensaje);
+
+
+    }
+}
