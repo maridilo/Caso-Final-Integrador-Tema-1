734e6d25970a2ec518eb4944c5698992f13b1181
maridilo <146822054+maridilo@users.noreply.github.com>
2024-02-25 18:21:02 +0100

    commit

--- ProyectoCronos/src/test/java/CronometroCosmicotest.java
+++ ProyectoCronos/src/test/java/CronometroCosmicotest.java
@@ -1,3 +1,11 @@
+import org.junit.jupiter.api.Test;
 
 public class CronometroCosmicotest {
+    @Test
+    public void testMonitorearvariables() {
+        double radiacion = 75.6;
+        double temperatura = 25.5;
+        double presion = 1013.2;
+        SistemadeAlertayMonitoreo.monitorearvariables(radiacion, temperatura, presion);
+    }
 }
